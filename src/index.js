6c6dcbbd9bb96aed9e43febbee969d0368c38226
teitonny <tonnytei4@gmail.com>
2022-11-10 15:17:40 +0300

    Added the list structure to the project

---
+++ src/index.js
@@ -0,0 +1,55 @@
+import _ from 'lodash';
+// import printMe from './print.js';
+import './style.css';
+
+const myList = document.querySelector('.to-do-list')
+
+const ToDoArray = [
+    {
+      description: "write code",
+      completed: true,
+      i: 0,
+    },
+    {
+      description: "read a novel",
+      completed: true,
+      i: 1,
+    },
+    {
+      description: "watch a movie",
+      completed: true,
+      i: 2,
+    },
+    {
+      description: "play with kids",
+      completed: true,
+      i: 3,
+    },
+];
+
+let appended = ' ';
+
+const appedTask =(array) => {
+    array.forEach(item => {
+        appended += `
+        <div class="to-do-pop">
+            <div class="checkbox">
+                <input
+                    type="checkbox"
+                    id="to-do-check"
+                    name="To-Do"
+                    value="Add"
+                />
+                
+                <label for="todo" class="my-label">${item.description}</label><br />
+            </div>
+            <i class="fa-solid fa-ellipsis-vertical"></i>
+        </div>
+      <hr />
+        
+        `
+    });
+    myList.innerHTML= appended;
+}
+
+appedTask(ToDoArray);
