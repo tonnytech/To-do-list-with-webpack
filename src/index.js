c220b55db2f7079d38b749a5118b14e2d31c6398
teitonny <tonnytei4@gmail.com>
2022-11-11 19:06:06 +0300

    removed eslint errors

--- src/index.js
+++ src/index.js
@@ -1,37 +1,36 @@
-import _ from 'lodash';
 // import printMe from './print.js';
 import './style.css';
 
-const myList = document.querySelector('.to-do-list')
+const myList = document.querySelector('.to-do-list');
 
 const ToDoArray = [
-    {
-      description: "write code",
-      completed: true,
-      i: 0,
-    },
-    {
-      description: "read a novel",
-      completed: true,
-      i: 1,
-    },
-    {
-      description: "watch a movie",
-      completed: true,
-      i: 2,
-    },
-    {
-      description: "play with kids",
-      completed: true,
-      i: 3,
-    },
+  {
+    description: 'write code',
+    completed: true,
+    i: 0,
+  },
+  {
+    description: 'read a novel',
+    completed: true,
+    i: 1,
+  },
+  {
+    description: 'watch a movie',
+    completed: true,
+    i: 2,
+  },
+  {
+    description: 'play with kids',
+    completed: true,
+    i: 3,
+  },
 ];
 
 let appended = ' ';
 
-const appedTask =(array) => {
-    array.forEach(item => {
-        appended += `
+const appedTask = (array) => {
+  array.forEach((item) => {
+    appended += `
         <div class="to-do-pop">
             <div class="checkbox">
                 <input
@@ -48,9 +47,9 @@ const appedTask =(array) => {
         </div>
       <hr />
         
-        `
-    });
-    myList.innerHTML= appended;
-}
+        `;
+  });
+  myList.innerHTML = appended;
+};
 
 appedTask(ToDoArray);
