6513e45387bc0996edff9397cb81a158551bc821
teitonny <tonnytei4@gmail.com>
2022-11-12 09:04:10 +0300

    added the delete button

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import _ from 'lodash';
-import getMyList, { addList,deleteListItems,updateListItem} from "./print.js";
+import getMyList, { addList, deleteListItems, updateListItems } from './print.js';
 import './style.css';
 
-const myList = document.querySelector('.to-do-list')
+const myList = document.querySelector('.to-do-list');
+const clearButton = document.querySelector("my-button");
 
-const getCreatedList = (item)=> {
-  return `
+const getCreatedList = (item) => `
   <div class="to-do-pop">
       <li class="checkbox" data-index="${item.index}"
        data-complete="${item.complete}">
@@ -16,51 +16,89 @@ const getCreatedList = (item)=> {
           class ="output-description" name ="${item.index}"
           value="${item.description}">
       </li>
-      <i class="fa-solid fa-ellipsis-vertical"></i>
+      <i class="icon-fa fa-solid fa-ellipsis-vertical"></i>
   </div>
 <hr />
   
   `;
-  };
 
-const appendTask =(array) => {
+const appendTask = (array) => {
   let appended = ' ';
 
   const sortedArray = array.sort((a, b) => a.index - b.index);
-    sortedArray.forEach(item => {
-        appended += getCreatedList(item);
-    });
-    myList.innerHTML= appended;
-}
+  sortedArray.forEach((item) => {
+    appended += getCreatedList(item);
+  });
+  myList.innerHTML = appended;
+};
 
 appendTask(getMyList());
 
-//adding to the list
+// adding to the list
 // const addToDo = document.querySelector('#add-todo')
-window.addEventListener("keydown", (e) => {
-  if(e.key === "Enter") {
+window.addEventListener('keydown', (e) => {
+  if (e.key === 'Enter') {
     const { value } = e.target;
-    console.log( "here");
-    if(!value?.trim()) return;
 
-    if (e.target.classList.contains("add-todo-input")) {
+    if (!value?.trim()) return;
 
+    if (e.target.classList.contains('add-todo-input')) {
       addList(value);
-      //when the list is empty
-      e.target.value = ""
+      // when the list is empty
+      e.target.value = '';
       appendTask(getMyList());
       return;
     }
 
-    if (e.target.classList.contains("output-description")) {
-      const mainElement = e.target.closest(".checkbox");
-    
+    if (e.target.classList.contains('output-description')) {
+      const mainElement = e.target.closest('.checkbox');
+
       const { index } = mainElement.dataset;
 
-      updateListItem(+index, value);
+      updateListItems(+index, value);
       appendTask(getMyList());
     }
   }
-
 });
 
+// Deleting list items
+
+myList.addEventListener("click", (e) => {
+  const itemClicked = e.target.closest(".icon-fa");
+
+  if (!itemClicked) return;
+
+  itemClicked.querySelector(".icon-fa").classList.add("fa-trash-can");
+
+  const trashIcon = itemClicked.querySelector(".fa-trash-can");
+
+  trashIcon?.addEventListener("click", () => {
+    
+    const majorElement = trashIcon.closest(".checkbox");
+    
+    const { index } = majorElement.dataset;
+
+    deleteListItems(+index);
+    appendTask(getMyList());
+  });
+});
+
+let RemoveIndex = [];
+
+myList.addEventListener("click", (e) => {
+  const activeElement = e.target.closest(".to-do-check");
+  if(!activeElement) return;
+  const majorElement = activeElement.closest("checkbox");
+  RemoveIndex = checkoffItem(majorElement);
+});
+
+clearButton.addEventListener("clicked", (e) => {
+  if(RemoveIndex.length > 0) {
+    RemoveIndex.forEach( (d) => {
+      deleteListItems(d);
+    });
+    appendTask(getMyList());
+  }
+
+  RemoveIndex = [];
+});
--- src/print.js
+++ src/print.js
@@ -1,52 +1,52 @@
 const getMyList = () => {
-    if (localStorage.getItem("todolist")) {
-      return JSON.parse(localStorage.getItem("todolist"));
-    }
-    return [];
+  if (localStorage.getItem('todolist')) {
+    return JSON.parse(localStorage.getItem('todolist'));
+  }
+  return [];
 };
 
 let myList = getMyList();
 
-export const addList = ( description, listArray = myList) => {
-    const list = {
-        description,
-        completed: false,
-        index: listArray.length + 1,
-    };
+export const addList = (description, listArray = myList) => {
+  const list = {
+    description,
+    completed: false,
+    index: listArray.length + 1,
+  };
 
-    myList.push(list);
+  myList.push(list);
 
-    // Adding to local storage
+  // Adding to local storage
 
-    localStorage.setItem("todolist", JSON.stringify(myList));
+  localStorage.setItem('todolist', JSON.stringify(myList));
 };
 
 export const deleteListItems = (index) => {
-    myList = myList.filter( (list) => list.index !== index);
+  myList = myList.filter((list) => list.index !== index);
 
-    const newList = myList.sort( (a,b) => a.index - b.index);
+  const newList = myList.sort((a, b) => a.index - b.index);
 
-    newList.forEach((obj,i) => {
-        obj.index = i +1;
-    });
+  newList.forEach((obj, i) => {
+    obj.index = i + 1;
+  });
 
-    localStorage.setItem("todolist", JSON.stringify(newList));
+  localStorage.setItem('todolist', JSON.stringify(newList));
 };
 
-export const updateListItems = ( index, information, TrueFalse = false ) => {
-    const updateListItem = myList.find( (listItem) => listItem.index === index);
-    const myListArrayIndex = updateListItem.index - 1;
+export const updateListItems = (index, information, TrueFalse = false) => {
+  const updateListItem = myList.find((listItem) => listItem.index === index);
+  const myListArrayIndex = updateListItem.index - 1;
 
-    const currentDescription = updateListItem.description;
+  const currentDescription = updateListItem.description;
 
-    if(information === currentDescription) return;
+  if (information === currentDescription) return;
 
-    updateListItem.description = information;
-    updateListItem.completed = TrueFalse;
+  updateListItem.description = information;
+  updateListItem.completed = TrueFalse;
 
-    mylist[myListArrayIndex] = itemToUpdate;
+  mylist[myListArrayIndex] = itemToUpdate;
 
-    localStorage.setItem("todolist", JSON.stringify(myList));
+  localStorage.setItem('todolist', JSON.stringify(myList));
 };
 
 export default getMyList;
